fcef9e6fd8033c09869cdff4315d7b6d11ac5eb7
Dmitry Kulakov <kulakoff@kilograpp.com>
2016-06-23 12:59:01 -0700

    Initial Commit

---
+++ Odessa temp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Odessa temp
+//
+//  Created by Dmitry Kulakov on 23.06.16.
+//  Copyright © 2016 kulakoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
